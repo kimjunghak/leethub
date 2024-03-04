890647b155d0824dbae6b32ff89e0c1bf0689bb1
kimjunghak <kboy1025@gmail.com>
2024-03-04 16:25:03 +0900

    Time: 127 ms (57.89%), Space: 32.2 MB (59.21%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.kt
+++ 0190-reverse-bits/0190-reverse-bits.kt
@@ -1,6 +1,10 @@
 class Solution {
     // you need treat n as an unsigned value
     fun reverseBits(n:Int):Int {
-        return Integer.reverse(n)
+        var result = 0
+        for (i in 0..<32) {
+            result = result shl 1 or (n shr i and 1)
+        }
+        return result
     }
 }
