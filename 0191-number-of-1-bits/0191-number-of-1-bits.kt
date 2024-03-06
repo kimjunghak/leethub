cc27dd03cadaea075e69a6b8648eeb670a652da5
kimjunghak <kboy1025@gmail.com>
2024-03-06 13:46:46 +0900

    Time: 149 ms (30.14%), Space: 33.5 MB (36.84%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.kt
+++ 0191-number-of-1-bits/0191-number-of-1-bits.kt
@@ -3,7 +3,7 @@ class Solution {
     fun hammingWeight(n:Int):Int {
         var weight = 0
         for (i in 0..31) {
-            if(n ushr i and 1 == 1) weight++
+            if(n shr i and 1 == 1) weight++
         }
 
         return weight
