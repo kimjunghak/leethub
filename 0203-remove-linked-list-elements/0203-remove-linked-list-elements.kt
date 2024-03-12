8802e1259b1df452df8d642340f57afe1bd1750c
kimjunghak <kboy1025@gmail.com>
2024-03-12 15:44:20 +0900

    Time: 206 ms (49.54%), Space: 38.2 MB (81.94%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.kt
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.kt
@@ -11,19 +11,7 @@ class Solution {
     fun removeElements(head: ListNode?, `val`: Int): ListNode? {
         if(head == null) return null
 
-        var currentHead = head
-        while (currentHead?.`val` == `val`) currentHead = currentHead.next
-
-        var currentNode = currentHead
-        var prevNode: ListNode? = null
-
-        while (currentNode != null) {
-            if(currentNode.`val` == `val`) prevNode?.next = currentNode.next
-            else prevNode = currentNode
-
-            currentNode = currentNode.next
-        }
-
-        return currentHead        
+        head.next = removeElements(head.next, `val`)
+        return if (head.`val` == `val`) head.next else head      
     }
 }
