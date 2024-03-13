1f6f53a18aff9b6f8797c665020d4695a0321522
kimjunghak <kboy1025@gmail.com>
2024-03-13 16:22:07 +0900

    Time: 210 ms (46.23%), Space: 37.8 MB (45.62%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.kt
+++ 0242-valid-anagram/0242-valid-anagram.kt
@@ -2,21 +2,16 @@ class Solution {
     fun isAnagram(s: String, t: String): Boolean {
         if(s.length != t.length) return false
 
-        val sMap = HashMap<Char, Int>()
-        val tMap = HashMap<Char, Int>()
+        val map = HashMap<Char, Int>()
 
         s.forEach {
-            sMap[it] = sMap.getOrDefault(it, 0) + 1
+            map[it] = map.getOrDefault(it, 0) + 1
         }
 
         t.forEach {
-            tMap[it] = tMap.getOrDefault(it, 0) + 1
+            map[it] = map.getOrDefault(it, 0) - 1
         }
 
-        for (key in sMap.keys) {
-            if(tMap[key] != sMap[key]) return false
-        }
-
-        return true
+        return map.values.all { it == 0 }
     }
 }
