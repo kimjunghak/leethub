dec85a031300faa354be40bf1928bebeada46d1c
kimjunghak <kboy1025@gmail.com>
2024-03-13 12:47:18 +0900

    Time: 164 ms (8.04%), Space: 33.3 MB (79.17%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.kt
+++ 0231-power-of-two/0231-power-of-two.kt
@@ -1,5 +1,14 @@
 class Solution {
     fun isPowerOfTwo(n: Int): Boolean {
-        return n > 0 && (n and (n-1)) == 0
+        if(n == 0) return false
+        if(n == 1) return true
+
+        var num = n
+        while (num != 2) {
+            if(num % 2 != 0) return false
+            num /= 2
+        }
+
+        return true
     }
 }
