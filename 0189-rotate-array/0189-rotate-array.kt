93895a4c3de4270e19ab09767ab4215e9b1928ec
kimjunghak <kboy1025@gmail.com>
2024-03-14 15:28:31 +0900

    Time: 323 ms (61.99%), Space: 50.7 MB (60.93%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.kt
+++ 0189-rotate-array/0189-rotate-array.kt
@@ -1,17 +1,9 @@
 class Solution {
     fun rotate(nums: IntArray, k: Int): Unit {
-        for (i in 1..k) {
-            val last = nums.last()
-            shift(nums)
-            nums[0] = last
-        }
+        val size = nums.size
+        val temp = IntArray(size)
 
-    }
-    
-    private fun shift(nums: IntArray) {
-        for (i in nums.lastIndex - 1 downTo 0) {
-            nums[i+1] = nums[i]
-        }
-
+        for (i in nums.indices) temp[(i+k)% size] = nums[i]
+        for (i in nums.indices) nums[i] = temp[i]
     }
 }
