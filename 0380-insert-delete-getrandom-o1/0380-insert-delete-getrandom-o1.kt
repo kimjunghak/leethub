16112fdf3de9d2beecfa72c43fdf586c9327f49d
kimjunghak <kboy1025@gmail.com>
2024-03-18 13:55:11 +0900

    Time: 780 ms (19.41%), Space: 104.2 MB (85.88%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.kt
@@ -0,0 +1,25 @@
+class RandomizedSet() {
+
+    val set = HashSet<Int>()
+    
+    fun insert(`val`: Int): Boolean {
+        return set.add(`val`)
+    }
+
+    fun remove(`val`: Int): Boolean {
+        return set.remove(`val`)
+    }
+
+    fun getRandom(): Int {
+        return set.random()
+    }
+
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = RandomizedSet()
+ * var param_1 = obj.insert(`val`)
+ * var param_2 = obj.remove(`val`)
+ * var param_3 = obj.getRandom()
+ */
