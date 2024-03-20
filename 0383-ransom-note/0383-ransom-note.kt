e2834b4de4e484e292ff70084ac79e9919acb630
kimjunghak <kboy1025@gmail.com>
2024-03-20 11:11:02 +0900

    Time: 249 ms (30.10%), Space: 37.1 MB (93.98%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.kt
+++ 0383-ransom-note/0383-ransom-note.kt
@@ -1,7 +1,9 @@
 class Solution {
     fun canConstruct(ransomNote: String, magazine: String): Boolean {
-        val map = magazine.groupingBy { it }.eachCount().toMutableMap()
-
+        val map = mutableMapOf<Char, Int>()
+        for (c in magazine) {
+            map[c] = map.getOrDefault(c, 0) + 1
+        }
         for (c in ransomNote) {
             if(map[c] == null || map[c] == 0) return false
             map[c] = map[c]!! - 1
